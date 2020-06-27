33915dd02386eadee7e97bee58928edc3cd85c9e
Carlson Santana Cruz <carlsonsantana@gmail.com>
2020-06-27 01:49:50 -0300

    Adicionar os testes do cadastro de supers

--- src/github.com/carlsonsantana/superheroapi/tests/api_test.go
+++ src/github.com/carlsonsantana/superheroapi/tests/api_test.go
@@ -1,7 +1,11 @@
 package tests
 
 import (
+	"encoding/json"
+	"github.com/carlsonsantana/superheroapi"
+	"io/ioutil"
 	"net/http"
+	"reflect"
 	"testing"
 )
 
@@ -20,5 +24,42 @@ func TestAPISuperNotFoundAPI(t *testing.T) {
 
 	if response.StatusCode != http.StatusFailedDependency {
 		t.Error("O webservice não esta devolvendo o código correto quando é informado um super que nao existe")
+	}
+}
+
+func TestAPISuperAdded(t *testing.T) {
+	response := requestPath("POST", "/super", map[string]string{
+		"name": "batman",
+	})
+
+	if response.StatusCode != http.StatusOK {
+		t.Error("O webservice não esta devolvendo o código correto quando é informado um super que nao existe")
+		return
+	}
+	body, _ := ioutil.ReadAll(response.Body)
+	apiResponseBody := &superheroapi.APIResponseBody{}
+	json.Unmarshal(body, apiResponseBody)
+	if len(apiResponseBody.Supers) == 0 {
+		t.Error("O webservice não retornou nenhum super pesquisando por batman")
+	}
+}
+
+func TestAPISuperNoDuplicate(t *testing.T) {
+	response1 := requestPath("POST", "/super", map[string]string{
+		"name": "superman",
+	})
+	body1, _ := ioutil.ReadAll(response1.Body)
+	apiResponseBody1 := &superheroapi.APIResponseBody{}
+	json.Unmarshal(body1, apiResponseBody1)
+
+	response2 := requestPath("POST", "/super", map[string]string{
+		"name": "superman",
+	})
+	body2, _ := ioutil.ReadAll(response2.Body)
+	apiResponseBody2 := &superheroapi.APIResponseBody{}
+	json.Unmarshal(body2, apiResponseBody2)
+
+	if !reflect.DeepEqual(apiResponseBody1, apiResponseBody2) {
+		t.Error("O webservice esta duplicando os supers cadastrados")
 	}
 }
